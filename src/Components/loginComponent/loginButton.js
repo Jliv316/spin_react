4b5a0b790709cc166988fb3daf3c6b5e28b50358
Jliv316 <jclivingston316@gmail.com>
2018-07-24 14:45:52 -0600

    successfully completed React on Rails Spotify Oath Process

--- src/Components/loginComponent/loginButton.js
+++ src/Components/loginComponent/loginButton.js
@@ -5,7 +5,9 @@ import Button from 'react-toolbox/lib/button/Button';
 
 const authorizeUrl = 'https://accounts.spotify.com/authorize'
 const clientId = 'd759774253a14825af09f813f6982145'
-const scope = 'playlist-read-private user-read-private user-read-email user-library-read user-top-read playlist-read-private user-top-read'
+const scope = 'playlist-read-private user-read-private user-read-email user-library-read user-top-read playlist-read-private user-top-read user-read-birthdate'
+const redirectURI = 'http://localhost:3166/auth/spotify/callback'
+const responseType = "code"
 
 class LoginButton extends Component {
   render() {
@@ -13,7 +15,7 @@ class LoginButton extends Component {
       <Button
         raised
         accent
-        href={`${authorizeUrl}?client_id=${clientId}&scope=${scope}`}
+        href={`${authorizeUrl}?client_id=${clientId}&redirect_uri=${redirectURI}&response_type=${responseType}&scope=${scope}`}
       >
         {/* <SpotifyIcon /> */}
         {' '}
